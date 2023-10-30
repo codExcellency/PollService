5e9b49b5423aa1a16f743a503a8156255928dc88
Minnavia <131753780+Minnavia@users.noreply.github.com>
2023-10-30 13:04:26 +0200

    Question REST

--- src/main/java/projekti/PollService/web/QuestionController.java
+++ src/main/java/projekti/PollService/web/QuestionController.java
@@ -1,12 +1,19 @@
 package projekti.PollService.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import projekti.PollService.domain.Question;
 import projekti.PollService.domain.QuestionRepository;
@@ -28,7 +35,25 @@ public class QuestionController {
 	public String saveQuestion(@ModelAttribute Question question) {
 		questionrepository.save(question);
 		return "redirect:/showpoll";
-		
+	}
+	
+	//REST
+	
+	//Get all questions
+	@GetMapping(value = "/questions")
+	public @ResponseBody List<Question> questionListRest(){
+		return (List<Question>) questionrepository.findAll();
 	}
 
+	//Get question by id
+	@GetMapping(value = "/questions/{id}")
+	public  @ResponseBody Optional<Question> findQuestionRest(@PathVariable("id") Long question_id){
+		return questionrepository.findById(question_id);
+	}
+	
+	//Add new question
+	@PostMapping(value = "/questions")
+	public @ResponseBody Question saveQuestionRest(@RequestBody Question question) {
+		return questionrepository.save(question);
+	}
 }
