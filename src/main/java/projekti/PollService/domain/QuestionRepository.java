6ca3262fac77615802c661d96940c5088aaf44f9
Neiwyth <user.example@mailing.com>
2023-11-01 11:47:07 +0200

    Error fix?

--- src/main/java/projekti/PollService/domain/QuestionRepository.java
+++ src/main/java/projekti/PollService/domain/QuestionRepository.java
@@ -2,6 +2,6 @@ package projekti.PollService.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface QuestionRepository extends CrudRepository<Question, Long>{
+public interface QuestionRepository extends CrudRepository<Question, Long> {
 
 }
