1cd32ccdc74b32c9fa475ffa701824d40a5caa61
Neiwyth <user.example@mailing.com>
2023-11-29 12:32:46 +0200

    Modified restcontroller

--- src/main/java/projekti/PollService/web/RestController.java
+++ src/main/java/projekti/PollService/web/RestController.java
@@ -31,54 +31,58 @@ public class RestController {
 
 	@Autowired
 	private QuestionRepository questionrepository;
-	
+
 	@Autowired
 	private AnswerRepository answerrepository;
 
-	
-	//POLLS
+	// POLLS
 
 	// Get all polls
 	@GetMapping(value = "/polls")
 	public @ResponseBody List<PollSummaryDTO> getPolls() {
 		List<Poll> polls = (List<Poll>) pollrepository.findAll();
-	    List<PollSummaryDTO> pollDTOs = polls.stream()
-	            .map(poll -> new PollSummaryDTO(poll.getPollId(), poll.getName(), poll.getDescription()))
-	            .collect(Collectors.toList());
-	    return pollDTOs;
+		List<PollSummaryDTO> pollDTOs = polls.stream()
+				.map(poll -> new PollSummaryDTO(poll.getPollId(), poll.getName(), poll.getDescription()))
+				.collect(Collectors.toList());
+		return pollDTOs;
 	}
 
+	// Get poll by id (with questions and answers)
+	@GetMapping(value = "/polls/{id}")
+	public @ResponseBody Optional<Poll> getPollById(@PathVariable("id") Long pollId) {
+		return pollrepository.findById(pollId);
+	}
 
 	// Get poll by id with questions (but not answers)
-    @GetMapping(value = "/polls/{id}/questions")
-    public @ResponseBody PollDetailDTO getPollById(@PathVariable("id") Long pollId) {
-        Poll poll = pollrepository.findById(pollId).orElse(null);
+	@GetMapping(value = "/polls/{id}/questions")
+	public @ResponseBody PollDetailDTO getPollQuestionsById(@PathVariable("id") Long pollId) {
+		Poll poll = pollrepository.findById(pollId).orElse(null);
 
-        if (poll != null) {
-            return new PollDetailDTO(
-                    poll.getPollId(),
-                    poll.getName(),
-                    poll.getDescription(),
-                    poll.getQuestions());
-        } else {
-            return null;
-       }
-    }
-	
-	//QUESTIONS
-	
-	//Get question by id
+		if (poll != null) {
+			return new PollDetailDTO(
+					poll.getPollId(),
+					poll.getName(),
+					poll.getDescription(),
+					poll.getQuestions());
+		} else {
+			return null;
+		}
+	}
+
+	// QUESTIONS
+
+	// Get question by id
 	@GetMapping(value = "/questions/{id}")
-	public @ResponseBody Optional<Question> getQuestionById(@PathVariable("id")Long questionId) {
+	public @ResponseBody Optional<Question> getQuestionById(@PathVariable("id") Long questionId) {
 		return questionrepository.findById(questionId);
 	}
-	
-	//ANSWERS
-	
-	//Post answer to a question
+
+	// ANSWERS
+
+	// Post answer to a question
 	@PostMapping(value = "/answers")
-	public @ResponseBody Answer postAnswer(@RequestBody Answer answer){
+	public @ResponseBody Answer postAnswer(@RequestBody Answer answer) {
 		return answerrepository.save(answer);
 	}
-	
+
 }
